ac01e3afa83ea937780c2da0d35f551458d85d9b
EdwinZarateP <emzp1994@gmail.com>
2024-11-15 14:33:24 -0500

    arreglo mapa

--- src/Componentes/Mapa/index.tsx
+++ src/Componentes/Mapa/index.tsx
@@ -20,7 +20,7 @@ interface MapaGlampingsProps {
 const MapaGlampings: React.FC<MapaGlampingsProps> = ({ glampings }) => {
   const apiKey = "150916424994462a85b859d4177a3cc7"; // Tu API Key de Thunderforest
 
-  // Crea un ícono personalizado con React Icons convertido a HTML
+  // Ícono personalizado
   const createCustomIcon = () =>
     L.divIcon({
       html: ReactDOMServer.renderToString(
@@ -30,7 +30,7 @@ const MapaGlampings: React.FC<MapaGlampingsProps> = ({ glampings }) => {
       ),
       iconSize: [40, 40],
       className: "custom-leaflet-icon",
-    });  
+    });
 
   return (
     <div className="mapa-contenedor">
@@ -38,20 +38,14 @@ const MapaGlampings: React.FC<MapaGlampingsProps> = ({ glampings }) => {
       <div className="mapa-wrapper">
         <MapContainer center={[5.5353, -73.3677]} zoom={7} className="mapa-interactivo">
           <TileLayer
-            // url={`https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=${apiKey}`}
-            // url={`https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=${apiKey}`}
-            // url={`https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=${apiKey}`}
             url={`https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=${apiKey}`}
-
-
-
             attribution='Maps &copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, Data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
           />
           {glampings.map((glamping) => (
             <Marker
               key={glamping.id}
               position={glamping.ubicacion}
-              icon={createCustomIcon()} // Usa el ícono personalizado
+              icon={createCustomIcon()}
             >
               <Popup>
                 <h3>{glamping.nombre}</h3>
