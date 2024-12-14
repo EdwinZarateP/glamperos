908c685818a19efed087fc05b7f7cb2d11a3718a
EdwinZarateP <emzp1994@gmail.com>
2024-12-13 22:09:08 -0500

    se ajusto scroll

--- src/Componentes/Header/index.tsx
+++ src/Componentes/Header/index.tsx
@@ -65,7 +65,6 @@ const Header: React.FC = () => {
   };
 
   const irAInicio = () => {
-    window.scrollTo({ top: 0, behavior: "auto" });
     setTimeout(() => navigate("/"), 1); // Retardo para permitir el scroll
   };
 
