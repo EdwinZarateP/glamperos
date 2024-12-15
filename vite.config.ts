b585e8f3951ba2ac08ed47f71b5a168f54b3b727
EdwinZarateP <emzp1994@gmail.com>
2024-12-15 07:22:01 -0500

    se configura el vite config para despliegue render

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base:'/glamperos/'
+    base:'/'
+    // base:'/glamperos/' activar este solo para despliegue local
 })
-
-
-
-
