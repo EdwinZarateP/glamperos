f2cda375ee5da8285225d02aa739fa87295dea17
EdwinZarateP <emzp1994@gmail.com>
2025-01-07 15:41:53 -0500

    se ajusto menu inferior

--- src/Componentes/MenuUsuariosInferior/index.tsx
+++ src/Componentes/MenuUsuariosInferior/index.tsx
@@ -2,50 +2,42 @@ import React from 'react';
 import { CiSearch } from "react-icons/ci";
 import { FaRegHeart, FaRegUser } from "react-icons/fa";
 import { TiMessage } from "react-icons/ti";
-import { Link } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import Cookies from "js-cookie";
 import './estilos.css';
 
 const MenuUsuariosInferior: React.FC = () => {
   const correoUsuario = Cookies.get("correoUsuario");
+  const navigate = useNavigate();  // Usamos useNavigate para redirigir sin recargar la página
+
+  const redirigir = (ruta: string) => {
+    if (!correoUsuario) {
+      // Si no existe el correo, redirige a la página de registrarse
+      navigate("/Registrarse");
+    } else {
+      // Si existe el correo, redirige a la ruta correspondiente
+      navigate(ruta);
+    }
+  };
 
   return (
     <div className="MenuUsuariosInferior">
-      {correoUsuario ? (
-        <>
-          <div className="MenuUsuariosInferior-icono">
-            <Link to="/" className="MenuUsuariosInferior-enlace">
-              <CiSearch className="MenuUsuariosInferior-iconoImagen" />
-              <span className="MenuUsuariosInferior-texto">Buscar</span>
-            </Link>
-          </div>
-          <div className="MenuUsuariosInferior-icono">
-            <Link to="/ListaDeseos" className="MenuUsuariosInferior-enlace">
-              <FaRegHeart className="MenuUsuariosInferior-iconoImagen" />
-              <span className="MenuUsuariosInferior-texto">Favoritos</span>
-            </Link>
-          </div>
-          <div className="MenuUsuariosInferior-icono">
-            <Link to="/" className="MenuUsuariosInferior-enlace">
-              <TiMessage className="MenuUsuariosInferior-iconoImagen" />
-              <span className="MenuUsuariosInferior-texto">Mensajes</span>
-            </Link>
-          </div>
-          <div className="MenuUsuariosInferior-icono">
-            <Link to="/GestionarCuenta" className="MenuUsuariosInferior-enlace">
-              <FaRegUser className="MenuUsuariosInferior-iconoImagen" />
-              <span className="MenuUsuariosInferior-texto">Perfil</span>
-            </Link>
-          </div>
-        </>
-      ) : (
-        <div className="MenuUsuariosInferior-icono">
-          <Link to="/Registrarse" className="MenuUsuariosInferior-enlace">
-            <FaRegUser className="MenuUsuariosInferior-iconoImagen" />
-            <span className="MenuUsuariosInferior-texto">Registrate o inicia sesión</span>
-          </Link>
-        </div>
-      )}
+      <div className="MenuUsuariosInferior-icono" onClick={() => redirigir("/")}>
+        <CiSearch className="MenuUsuariosInferior-iconoImagen" />
+        <span className="MenuUsuariosInferior-texto">Buscar</span>
+      </div>
+      <div className="MenuUsuariosInferior-icono" onClick={() => redirigir("/ListaDeseos")}>
+        <FaRegHeart className="MenuUsuariosInferior-iconoImagen" />
+        <span className="MenuUsuariosInferior-texto">Favoritos</span>
+      </div>
+      <div className="MenuUsuariosInferior-icono" onClick={() => redirigir("/")}>
+        <TiMessage className="MenuUsuariosInferior-iconoImagen" />
+        <span className="MenuUsuariosInferior-texto">Mensajes</span>
+      </div>
+      <div className="MenuUsuariosInferior-icono" onClick={() => redirigir("/GestionarCuenta")}>
+        <FaRegUser className="MenuUsuariosInferior-iconoImagen" />
+        <span className="MenuUsuariosInferior-texto">Perfil</span>
+      </div>
     </div>
   );
 };
