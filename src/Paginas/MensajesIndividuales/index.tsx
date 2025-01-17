bacfb5a2b03d6497132ecf7b102446551b24da6e
EdwinZarateP <emzp1994@gmail.com>
2025-01-16 19:44:57 -0500

    diseños chat

--- src/Paginas/MensajesIndividuales/index.tsx
+++ src/Paginas/MensajesIndividuales/index.tsx
@@ -6,7 +6,7 @@ function MensajesIndividuales() {
   return (
     <div className='MensajesIndividuales-contenedor'>
       <HeaderIcono descripcion="Glamperos" />   
-      <div className="ConversacionesMoviles"><ConversacionesMoviles/></div>      
+      <ConversacionesMoviles/>     
 
     </div>
   );
