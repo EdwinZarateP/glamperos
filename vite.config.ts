56b7861b5fc27f769544e2e4b4a9a3c0ffc96ec3
EdwinZarateP <emzp1994@gmail.com>
2025-02-14 06:04:42 -0500

    se vuelve a configurar vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', 
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Evita dividir código en muchos archivos (mejor para prerenderizado)
+      },
+    },
   },
   server: {
     port: 3000,
+    open: true
+  },
+  preview: {
+    port: 5000,
     open: true
   }
 })
