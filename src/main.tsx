b4489f3adb80c90eddd79c2a386015c2e0aec67e
EdwinZarateP <emzp1994@gmail.com>
2025-02-14 05:24:03 -0500

    se ajusta para prerender

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./Paginas/App/index";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { HelmetProvider } from "react-helmet-async"; // 👈 Importar HelmetProvider aquí
+import { HelmetProvider } from "react-helmet-async"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* Proveedor de autenticación de Google */}
     <GoogleOAuthProvider clientId="870542988514-rbpof111fdk5vlbn75vi62i06moko46s.apps.googleusercontent.com">
-      <HelmetProvider> {/* 👈 Mueve HelmetProvider aquí */}
+      <HelmetProvider> 
         <App />
       </HelmetProvider>
     </GoogleOAuthProvider>
